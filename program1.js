acc7a327b42b9a1316ce7ff72ea287c2ff3c3dc3
saumyata <saumyata@github.com>
2024-04-26 13:12:57 +0530

    done

--- program1.js
+++ program1.js
@@ -1,9 +1,22 @@
 function smallestMissingPositiveInteger(nums) {
+  // Handle the base case where the list is empty
+  if (!nums.length) return 1;
 
-
-    // Implement the function smallest_missing_positive_integer
-  
+   const positiveNumbers = new Set();
+  for (const num of nums) {
+    if (num > 0) {
+      positiveNumbers.add(num);
+    }
   }
+
+  for (let i = 1; i <= positiveNumbers.size + 1; i++) {
+    if (!positiveNumbers.has(i)) {
+      return i;
+    }
+  }
+
+  return positiveNumbers.size + 1;
+}
   
   module.exports = smallestMissingPositiveInteger;
 
